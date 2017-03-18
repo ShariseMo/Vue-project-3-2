2f3a51acf0311df9d7cf0f3bda5b8eb36a421df1
ShariseMo <1009708910@qq.com>
2017-03-18 18:02:20 +0800

    2017-3-18

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,8 +10,70 @@
             <li>风格</li>
             <div class="clear"></div>
         </ul>
-        <ul class="type_box2"></ul>
-    
+        <ul class="type_box2">
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <li><img src="../assets/img/lamp01.png"></li>
+            <div class="clear"></div>
+        </ul>
+        <div class="weekly_new">
+            <h3><img src="../assets/img/new_title.jpg"></h3>
+            <ul>
+                <li><img src="../assets/img/new_img1.jpg" alt=""></li>
+                <li><img src="../assets/img/new_img1.jpg" alt=""></li>
+                <li><img src="../assets/img/new_img1.jpg" alt=""></li>
+                <li><img src="../assets/img/new_img1.jpg" alt=""></li>
+                <div class="clear"></div>
+            </ul>
+        </div>
+        <div class="hot_style">
+            <h3 class="box_title"><img src="../assets/img/hot_style.jpg"></h3>
+            <ul>
+                <li><img src="../assets/img/style01.png" alt=""></li>
+                <li><img src="../assets/img/style02.png" alt=""></li>
+                <li><img src="../assets/img/style03.png" alt=""></li>
+            </ul>
+        </div>
+        <div class="reco_box">
+            <h3 class="box_title"><img src="../assets/img/recommand.jpg"></h3>
+            <ul class="pro_list">
+                <li class="pro_item">
+                    <span><img src="../assets/img/lamp01.png"></span>
+                    <p class="pro_name">奥萨美式风格吊灯</p>
+                    <p class="price_sale">零售价：199.00元</p>
+                    <p class="price_discount">优惠价：188.00元</p>
+                </li>
+                <li class="pro_item">
+                    <span><img src="../assets/img/lamp02.png"></span>
+                    <p class="pro_name">奥萨美式风格吊灯</p>
+                    <p class="price_sale">零售价：199.00元</p>
+                    <p class="price_discount">优惠价：188.00元</p>
+                </li>
+                <li class="pro_item">
+                    <span><img src="../assets/img/lamp03.png"></span>
+                    <p class="pro_name">奥萨美式风格吊灯</p>
+                    <p class="price_sale">零售价：199.00元</p>
+                    <p class="price_discount">优惠价：188.00元</p>
+                </li>
+                <li class="pro_item">
+                    <span><img src="../assets/img/lamp02.png"></span>
+                    <p class="pro_name">奥萨美式风格吊灯</p>
+                    <p class="price_sale">零售价：199.00元</p>
+                    <p class="price_discount">优惠价：188.00元</p>
+                </li>
+                <li class="pro_item">
+                    <span><img src="../assets/img/lamp02.png"></span>
+                    <p class="pro_name">奥萨美式风格吊灯</p>
+                    <p class="price_sale">零售价：199.00元</p>
+                    <p class="price_discount">优惠价：188.00元</p>
+                </li>
+            </ul>
+        </div>
     </div>
     <!-- <router-view></router-view> -->
 </template>
@@ -32,14 +94,13 @@
 </script>
 
 <style lang="scss" scoped>
-    .page{background:#F1F2F6;}
+    .page{overflow:hidden;background:#F1F2F6;}
     .banner{
       height:3.5rem;
       img{width:100%;height:100%;}
     }
     .type_box1{
         margin-top:0.2rem;
-        border-bottom:1px solid #eee;
         background:#fff;
         li{
             width:25%;
@@ -52,4 +113,34 @@
             border-bottom:3px solid #ff6600;
         }
     }
+    .type_box2{
+        li{
+            float:left;
+            width:25%;
+            border:0.01rem solid #ccc;
+            background:#fff;
+            img{width:70%;margin:0.3rem 15%;}
+        }
+    }
+    .weekly_new{
+        background:url(../assets/img/new_bg.jpg);
+        text-align:center;
+        padding:0.15rem;
+        li{
+            float:left;
+            width:50%;
+            img{width:95%;}
+        }
+    }
+  
+    h3.box_title{
+        margin:0.3rem 0 0.1rem 0;
+        text-align:center;
+        img{width:30%;}
+    }
+    .reco_box{
+        text-align:center;
+        font-size:0.2rem;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-import './assets/css/public.css'
+import './assets/css/public.scss'
 import './assets/js/public.js'
 import Vue from 'vue'
 import App from './App'
