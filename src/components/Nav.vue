29b2c9f6460ebc9ed74b9ca67eb351e45ed8a2f0
王世隆 <547463736@qq.com>
2017-03-19 23:58:49 +0800

    Fix

---
+++ src/components/Nav.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="nav_bottom">
+    <ul>
+      <li>
+        <router-link to="/layouts/home"><img src="../assets/img/icon/home.png"></router-link>
+      </li>
+      <li>
+        <router-link to="/layouts/banner"><img src="../assets/img/icon/camp.png"></router-link>
+      </li>
+      <li>
+        <router-link to="/layouts/banner"><img src="../assets/img/icon/pic.png"></router-link>
+      </li>
+      <li>
+        <router-link to="/layouts/banner"><img src="../assets/img/icon/account.png"></router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "nav",
+    data: function () {
+      return {
+
+      }
+    },
+  };
+
+</script>
+
+<style lang="scss" scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  .nav_bottom {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    background: #fff;
+    ul {
+      margin-left: -10px;
+    }
+    li {
+      float: left;
+      width: 25%;
+      padding: 5px 0;
+      text-align: center;
+      border-right: 1px solid #fff;
+      background: #eee;
+      img {
+        width: 45%;
+      }
+    }
+    li:last-child {
+      border: 0;
+    }
+  }
+
+</style>
---
+++ src/pages/layouts.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="layouts">
+   <router-view></router-view>
+   <ps-nav/>
+  </div>
+
+</template>
+
+<script>
+  import psNav from '@/components/Nav';
+  export default {
+    name: "home",
+    components: {
+      psNav
+    },
+    data() {
+      return {
+        msg: "这是首页"
+      }
+    },
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles/mixins';
+  .page {
+    overflow: hidden;
+    background: #F1F2F6;
+  }
+
+  .banner {
+    height: 3.5rem;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .type_box1 {
+    margin-top: 0.2rem;
+    background: #fff;
+    @include flexbox();
+    li {
+      width: 25%;
+      padding: 5px 0;
+      text-align: center;
+    }
+    .type_active {
+      color: #ff6600;
+      border-bottom: 3px solid #ff6600;
+    }
+  }
+
+  .type_box2 {
+    @include flexbox();
+    @include flex-wrap(wrap);
+    li {
+      width: 25%;
+      border: 0.01rem solid #ccc;
+      background: #fff;
+      img {
+        width: 70%;
+        margin: 0.3rem 15%;
+      }
+    }
+  }
+
+  .weekly_new {
+    background: url(../assets/img/new_bg.jpg);
+    text-align: center;
+    padding: 0.15rem;
+    ul {
+    @include flex-wrap(wrap);
+    @include flexbox();
+      li {
+        width: 50%;
+        img {
+          width: 95%;
+        }
+      }
+    }
+  }
+
+  h3.box_title {
+    margin: 0.3rem 0 0.1rem 0;
+    text-align: center;
+    img {
+      width: 30%;
+    }
+  }
+
+  .reco_box {
+    text-align: center;
+    font-size: 0.2rem;
+  }
+
+</style>
